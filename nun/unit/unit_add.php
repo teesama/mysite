eabad2b11843290ab444fd64307c6dbd378ffff9
maythanun <5829010028@users.noreply.github.com>
2020-03-11 15:55:23 +0700

    nun

--- nun/unit/unit_add.php
+++ nun/unit/unit_add.php
@@ -10,35 +10,10 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/mycss.css">
+    <link rel="stylesheet" href="../css/mycss.css">
 </head>
 <body>
- <!-- navbar -->
-<meta charset="utf-8">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="../index.php">ระบบจัดการการแข่งขันกีฬาภายในวิทยาลัยเทคนิคสัตหีบ</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-    </ul>
-    <form class="form-inline ">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">ชื่อผู้ใช้ <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">|</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#logout">Logout</a>
-      </li>
-    </ul>
-    </form>
-  </div>
-</nav>
 
 <?php
 require_once '../connect.php';
@@ -95,7 +70,6 @@ if(isset($_POST['btnSubmitEmp'])){
     }
  ?>
 </style>
-  <div class="container" style="border:1px white solid; width:1700px; height: 600px;  background-color:white; opacity:1.5;">
 
 
 <div class="container" style="width:700px;margin-top:40px;   ">
